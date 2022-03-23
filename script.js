9a7e1f7178c76f1186d768deba5830838abb68b9
StuMartin22 <stuart.martin@rocketmail.com>
2022-03-23 18:01:07 -0500

    created prompt for password length

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var userSelections = {
   upperCase: false,
@@ -22,14 +22,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-if (passwordLength <8) {
-  alert("please enter more than 8 characters")
-  return null
-}
+// if (passwordLength <8) {
+//   alert("please enter more than 8 characters")
+//   return null
+// }
 
 function generatePassword (){
-  var passLength = 
-  return password
+  var passLength = prompt("Please enter a number between 8 and 128 to set how long you want your password.")
+    if (passLength < 8 || passLength > 128) {
+      alert("Please... Make it between 8 and 128 characters...")
+      generatePassword()
+    }
+ // return password
 }
 
 //1. create variables to save input from prompts
